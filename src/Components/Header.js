c482f30d59a623327b6498b85b4fdb8e572de000
Anna <gstudentseo@gmail.com>
2022-10-27 11:17:13 +0200

    Created react-app and added 2 folders Components- Header.js and Pages. Install bootstrap

---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Container, Navbar } from "react-bootstrap";
+
+export default class Header extends Component {
+  render() {
+    return(
+      <Navbar>
+        <Container>
+          <Navbar.brand href = "/">
+            <img 
+                src = {}
+                height = "30"
+                width = "30"
+                c
+                        />
+
+          </Navbar.brand>
+        </Container>
+      </Navbar>
+    )
+  }
+}
