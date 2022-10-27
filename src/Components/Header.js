9ee2d8d550fb5971ae245485c12fb3f924e31330
Anna <gstudentseo@gmail.com>
2022-10-27 22:41:00 +0200

    React router. Page navigation

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
-import { Container, FormControl, Nav, Navbar, Button, Form } from "react-bootstrap";
+import { Nav,
+  Navbar, 
+  FormControl, 
+  Container, 
+  Form, 
+  Button 
+} from "react-bootstrap";
 import logo from './logo192.png';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Home from "../Pages/Home";
+import Contacts from "../Pages/Contacts";
+import About from "../Pages/About";
+import Blog from "../Pages/Blog";
 
 export default class Header extends Component {
   render() {
     return (
       <>
+      <Router>
         <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand href="/">
+            <Navbar.Brand as = {Link} to = "/">
               <img
                 src={logo}
                 height="30"
@@ -20,10 +33,10 @@ export default class Header extends Component {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link href="/" > Home </Nav.Link>
-                <Nav.Link href="/about" > About us </Nav.Link>
-                <Nav.Link href="/contacts" > Contacts </Nav.Link>
-                <Nav.Link href="/blog" > Blog </Nav.Link>
+                <Nav.Link as = {Link} to = "/" > Home </Nav.Link>
+                <Nav.Link as = {Link} to = "/about" > About us </Nav.Link>
+                <Nav.Link as = {Link} to = "/contacts" > Contacts </Nav.Link>
+                <Nav.Link as = {Link} to = "/blog" > Blog </Nav.Link>
               </Nav>
               <Form className="form-inline">
                 <FormControl
@@ -36,6 +49,14 @@ export default class Header extends Component {
             </Navbar.Collapse>
           </Container>
         </Navbar>
+        
+          <Routes>
+            <Route path ="/" element = {<Home/>}/>
+            <Route path ="/about" element = {<About/>}/>
+            <Route path ="/contacts" element = {<Contacts/>}/>
+            <Route path ="/blog" element = {<Blog/>}/>
+          </Routes>
+        </Router>
       </>
     );
   }
---
+++ src/Pages/Blog.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className = "mt-5">Hello Blog</h1>
+      </div>
+    )
+  }
+}
